9f148213f088400f47ec49c7ba82815b2d96dab6
Gergő Kiss <61420724+gregkiss98@users.noreply.github.com>
2020-12-08 18:56:03 +0100

    Add files via upload

--- src/components/Deals.js
+++ src/components/Deals.js
@@ -16,10 +16,11 @@ function App() {
   }, [])
 
   return(
-   <div>
+   <div class="deals">
     <h1>Steam Deals</h1>
+    <h2>Hottest games right now</h2>
     <ul>
-      {list.map(title => <li key={title.title}>{title.title}</li>)}
+      {list.map(title => <li key={title.storeid}><img src={title.thumb}></img> Game name: {title.title} | Sale Price: {title.salePrice} | Normal Price: {title.normalPrice}</li>)}
     </ul>
 
   </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { getList } from '../services/list';
- 
+
 class Search extends Component {
   render() {
     return (
       <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for Games.."></input>
+      
     );
   }
 }
